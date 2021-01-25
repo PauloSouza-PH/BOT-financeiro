0011c88b1e7149d6daefecf870f5a2a02a8d0d07
PauloSouza-GV <paulosouza@grupovila.com.br>
2021-01-24 23:34:19 -0300

    adicionando arquivos iniciais

---
+++ API/APINovaDax.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+
+module.exports = {
+
+    async getCoinMyAcc(){
+    
+    },
+
+    async getLastPriceMarket(){
+
+    },
+
+    async getHistoryPrice(){
+
+    }
+};
